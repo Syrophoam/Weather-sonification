f68e3d94573eba66c684dc6720859ef7b34d81b4
james fullerton <jamesfullerton2006@gmail.com>
2024-08-26 14:20:56 +1200

    Initial Commit

---
+++ CURLforpeople/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CURLforpeople
+//
+//  Created by syro Fullerton on 26/08/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
